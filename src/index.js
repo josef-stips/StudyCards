4d7ff53aaf066f6b7c9e8b08efae654f77d86089
josef-stips <93494931+josef-stips@users.noreply.github.com>
2023-01-28 15:14:57 +0100

    window size changes

--- src/index.js
+++ src/index.js
@@ -18,8 +18,8 @@ app.on('ready', () => {
 const createWindow = () => {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1000,
+        height: 800,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             devTools: false
