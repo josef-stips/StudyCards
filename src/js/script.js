aee902c6a3450720949df113d673d55766450fe2
josef-stips <93494931+josef-stips@users.noreply.github.com>
2023-03-08 15:22:30 +0100

    created new file

--- src/js/script.js
+++ src/js/script.js
@@ -1937,32 +1937,6 @@ function ResetApp() {
     SetAppColorsToDefault();
 };
 
-//Sends Mail to developer
-function SendMail() {
-    // let email = document.getElementById('email').value; //display = none;
-    // let subject = document.getElementById('subject').value; //display = none;
-    let name = document.getElementById('name').value;
-    let user_message = document.getElementById('message').value;
-    let body = `name - ${name}<br/>message:<br/><br/>${user_message}`;
-
-    if (mail_name_field.value != "" && 
-        mail_message_field.value != "") {
-
-        Email.send({
-            SecureToken : "50ae5256-e4e9-4700-b42b-fafc3cd150ec",
-            To : 'josef.stips@sgw-schule.de',
-            From : 'josefstips@gmx.de',
-            Subject : 'Sended from User',
-            Body : body
-        })
-        .then(
-          message => SetUpSmallPopUp('ok' , 'cool' , 'block' , 'block' , 'Email was successfully send to the developer')
-        );
-    } else {
-        SetUpSmallPopUp('okay' , 'oh!' , 'block' , 'block' , 'Fill out the text fields!');
-    };
-};
-
 //clears the formular after the email was sended 
 function clearFormular() {
     mail_name_field.value = "";
